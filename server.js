5dba12203868f67cc24895d39b7cf910f420c1b6
ayham-kattan <aak201610885@gmail.com>
2022-09-19 04:52:41 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const fileupload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+app.use(fileupload({
+  useTempFiles: true,
+}));
+//Routes
+app.use('/user',require('./routes/userRouter'));
+app.use('/api',require('./routes/categoryRouter'));
+app.use('/api',require('./routes/upload'));
+app.use('/api',require('./routes/productRouter'));
+app.use('/api',require('./routes/paymentRouter'));
+//connect to mongoDB
+const URI = process.env.MONGO_URI;
+mongoose.connect(URI, { useNewUrlParser: true },
+).then(()=>{console.log('connected')}).catch(err=>{console.log(err)})
+app.get('/', (req, res) => {
+  res.json({msg:"Hello World"});
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
